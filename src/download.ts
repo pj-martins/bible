80a336b0746534f42a2d3469d07655fc890f0cd0
PJ Martins <pj_martins@outlook.com>
2025-04-11 21:36:32 -0700

    add verses

--- src/download.ts
+++ src/download.ts
@@ -1,34 +1,34 @@
 import axios from "axios";
-import { Client, QueryResult } from "pg";
 import { parse } from 'node-html-parser';
 import fs from 'fs';
 import path from 'path';
 
 (async () => {
-    const db = new Client({ host: 'petjak.com', user: 'postgres', password: 'r3c!pe', database: 'postgres' })
     const baseUrl = 'https://www.bible.com';
+    const versionMap: Array<any> = [];
     for (const l of ['eng', 'afr']) {
-        if (!fs.existsSync(l)) {
-            fs.mkdirSync(l);
+        const p = path.join('books', l);
+        if (!fs.existsSync(p)) {
+            fs.mkdirSync(p);
         }
         let res = await axios.get(`${baseUrl}/api/bible/versions?language_tag=${l}&type=all`);
         for (const v of res.data.response.data.versions) {
-		console.log("V:", v);
-		continue;
-//	      console.log(v.abbreviation);
-//            if (l == "eng") {
-                if (!['NIV11', 'ESV', 'ASV', 'AMP', 'CEV', 'GNT', 'KJV', 'MSG', 'NET', 'NKJV', 'NLT', 'ABA', 'AFR20', 'AFR53', 'AFR83', 'CAB23'].includes(v.abbreviation)) {
-		    continue;
-                }
-  //          }
-            if (!fs.existsSync(path.join(l, v.abbreviation))) {
-                fs.mkdirSync(path.join(l, v.abbreviation));
+            versionMap.push({
+                language: l,
+                abbreviation: v.abbreviation,
+                title: v.title
+            });
+            if (!['NIV11', 'ESV', 'ASV', 'AMP', 'CEV', 'GNT', 'KJV', 'MSG', 'NET', 'NKJV', 'NLT', 'ABA', 'AFR20', 'AFR53', 'AFR83', 'CAB23'].includes(v.abbreviation)) {
+                continue;
+            }
+            if (!fs.existsSync(path.join(p, v.abbreviation))) {
+                fs.mkdirSync(path.join(p, v.abbreviation));
             }
             const books = await axios.get(`${baseUrl}/api/bible/version/${v.id}`);
-//            console.log("B:", books);
             let i = 1;
             for (const b of books.data.books) {
-                const fileName = path.join(l, v.abbreviation, `${l}_${v.abbreviation}_${i++}_${b.usfm}.json`);
+                console.log(v.abbreviation, b.human);
+                const fileName = path.join(p, v.abbreviation, `${l}_${v.abbreviation}_${i++}_${b.usfm}.json`);
                 if (fs.existsSync(fileName)) continue;
                 const currBook: any = {
                     abbreviation: b.usfm,
@@ -36,21 +36,32 @@ import path from 'path';
                     chapters: [],
                     language: l,
                 };
-                // curr.books.push(currBook);
                 for (const c of b.chapters) {
                     const url = `${baseUrl}/_next/data/odPN62GeIjyJdC15r-ASU/en/bible/${v.id}/${c.usfm}.${v.abbreviation}.json?versionId=${v.id}&usfm=${c.usfm}.${v.abbreviation}`;
                     const verses = await axios.get(url)
-                    const parsed = parse(verses.data.pageProps.chapterInfo.content)
-                    console.log(v.abbreviation, c.usfm, parsed.querySelectorAll(`span[class='content']`)[1].innerText);
+                    const parsed = parse(verses.data.pageProps.chapterInfo.content);
+                    const mappedVerses = parsed.querySelectorAll(`span[class='content']`).map(x => ({
+                        verse: x.parentNode.getAttribute('data-usfm'),
+                        text: x.innerText
+                    }));
+                    // for (let i = 0; i < mappedVerses.length; i++) {
+                    //     if (mappedVerses[i].verse) {
+                    //         mappedVerses[i].verse = mappedVerses[i].verse?.split('.').pop()
+                    //     } else if (i > 0) {
+                    //         mappedVerses[i].verse = mappedVerses[i - 1].verse;
+                    //     } else {
+                    //         mappedVerses[i].verse = '1';
+                    //     }
+                    // }
                     currBook.chapters.push({
                         chapter: c.usfm,
-                        texts: parsed.querySelectorAll(`span[class='content']`).map(x => x.innerText)
+                        verses: mappedVerses
                     });
                 }
                 fs.writeFileSync(fileName, JSON.stringify(currBook));
             }
-
         }
     }
+    // fs.writeFileSync('version-map.json', JSON.stringify(versionMap));
     process.exit(1);
 })();
--- src/populate-db.ts
+++ src/populate-db.ts
@@ -3,10 +3,39 @@ import fs from 'fs';
 import path from 'path';
 
 (async () => {
+	const db = new Client({ host: 'petjak.com', user: 'postgres', password: 'r3c!pe', database: 'bible' });
+	await db.connect();
+	let curr = await db.query('select * from bibles');
+	const versionMap: Array<{ language: string, abbreviation: string, title: string }> = JSON.parse(fs.readFileSync('version-map.json').toString());
 	for (const l of fs.readdirSync('books')) {
 		for (const v of fs.readdirSync(path.join('books', l))) {
-			console.log("V:", v);
+			let bible = curr.rows.find((r) => r.language == l && r.abbreviation == v);
+			if (!bible) {
+				await db.query(`
+insert into bibles (language, abbreviation, title)
+select '${l}', '${v}', '${versionMap.find((x) => x.language == l && x.abbreviation == v)?.title}'
+`);
+				curr = await db.query('select * from bibles');
+				bible = curr.rows.find((r) => r.language == l && r.abbreviation == v);
+			}
+
+			let books = await db.query(`select * from books where bible_id = ${bible.bible_id}`);
+
+			for (const b of fs.readdirSync(path.join('books', l, v))) {
+				const content = JSON.parse(fs.readFileSync(path.join('books', l, v, b)).toString());
+				const ind = +b.split('_')[2];
+				let book = books.rows.find((b) => b.abbreviation == content.abbreviation);
+				if (!book) {
+					await db.query(`
+						insert into books (bible_id, abbreviation, book_name, order_index)
+						select ${bible.bible_id}, '${content.abbreviation}', '${content.name}', ${ind}
+					`);
+					books = await db.query(`select * from books where bible_id = ${bible.bible_id}`);
+					book = books.rows.find((b) => b.abbreviation == content.abbreviation);
+				}
+			}
 		}
 	}
+	await db.end();
 	process.exit(1);
 })();
